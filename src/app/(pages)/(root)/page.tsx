01a4c4784511398170908ff84656b0a9253d7032
T_Mori <morit0301@gmail.com>
2024-12-15 18:32:41 +0900

    コンテンツ一覧の実装

--- src/app/(pages)/(root)/page.tsx
+++ src/app/(pages)/(root)/page.tsx
@@ -1,11 +1,26 @@
 import Hero from "./_components/hero";
+import Gallery from "./_components/gallery";
+import { fetchRepoData } from "@/utils/api";
+import { PROJECTS } from "../../../../_data/contents";
 
-export default function Home() {
+export default async function Home() {
+  const projects = await Promise.all(
+    PROJECTS.map(async (project) => {
+      const data = await fetchRepoData(project.repoUrl);
+      return {
+        owner: data.owner,
+        slug: data.slug,
+        repoName: data.repoName,
+        topics: data.topics,
+        imageURL: project.imageURL,
+      };
+    })
+  );
+
   return (
     <main className="space-y-32 pt-16">
-      {/* <p>ヒーローセクション</p> */}
       <Hero />
-      <p>ギャラリー一覧</p>
+      <Gallery projects={projects.reverse()} />
     </main>
   );
 }
