95d0a33d3fe59ad3a8f27e4a6b9bc56a299fa315
T_Mori <morit0301@gmail.com>
2024-12-15 16:34:47 +0900

    サイトレイアウトの作成

---
+++ src/app/(pages)/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "./_components/hero";
+
+export default function Home() {
+  return (
+    <main className="space-y-32 pt-16">
+      {/* <p>ヒーローセクション</p> */}
+      <Hero />
+      <p>ギャラリー一覧</p>
+    </main>
+  );
+}
